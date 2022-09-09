467c96256957e65f52ad4f64561f39f2b7c6e9f9
Hoài Bảo <hoaibao572@gmail.com>
2022-09-09 17:35:02 +0700

    event in react

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -4,13 +4,32 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Nam',
+        address: 'Viet Nam',
+        age: 26
+    };
+
+    handeClick(event) {
+        // console.log('Click me ^^');
+        // console.log(event.target); // <button>Click Me !</button>
+        // console.log(event);
+        console.log('My name is ', this.state.name)
+    }
+
+    handleOnMouseOver(event) {
+        // console.log(event);
+        console.log(event.pageX);
+    }
+
     // JSX
     render() {
         return (
             <div>
-                My First Component
-
-                {Math.random()}
+                My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handeClick}>Click Me!</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover Me!</button>
             </div>
         );
     }
